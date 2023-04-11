83b467808195f0f20ca7fac914be4e405a596d46
LeoDu <211250066@smail.nju.edu.cn>
2023-04-11 11:13:02 +0800

    fix: wrong url

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -3,7 +3,7 @@ import { SERVER_ADDR } from "../config";
 
 // 封装一下发起请求的部分, 便于本地测试时调用
 function request(args) {
-    args.url = SERVER_ADDR + args.path
+    args.url = SERVER_ADDR + args.url
     return axios.request(args)
 }
 
